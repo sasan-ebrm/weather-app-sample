f2193f4383c0bdc972e609e7f7ba9c49268a0e00
Sasan Ebrahimi <sasan.ebrm@gmail.com>
2023-01-08 10:52:44 +0100

    Add Hilt dependencies and api module

--- app/src/main/java/com/sebrahimi/weatherappsample/MainActivity.kt
+++ app/src/main/java/com/sebrahimi/weatherappsample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.sebrahimi.weatherappsample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,7 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.sebrahimi.weatherappsample.ui.theme.WeatherAppSampleTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/sebrahimi/weatherappsample/WeatherApplication.kt
@@ -0,0 +1,7 @@
+package com.sebrahimi.weatherappsample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WeatherApplication: Application()
